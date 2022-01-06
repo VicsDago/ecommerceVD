1076352d4406d53dd3dacf004bd58c3cbc9bf35c
VicsDago <vicsdago@gmail.com>
2022-01-06 16:25:36 -0300

    Desafio 1

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function NavBar (){
+    return(
+    <>
+    <div className="navBar">
+        <div className="navBar">
+        <img src="#" alt="BRAND" />
+        <ul>
+            <li> <a href="#">quienes somos </a> </li>
+            <li> <a href="#">productos </a> 
+                <ul>
+                <li><a href="#">producto 1</a></li> 
+                <li><a href="#">producto 2</a></li>
+                <li><a href="#">producto 3</a>
+                    <ul>
+                    <li><a href="#">subproducto 1</a></li> 
+                    <li><a href="#">subproducto 2</a></li>
+                    <li><a href="#">subproducto 3</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </li>
+            <li> <a href="#">contacto </a> </li>
+        </ul> 
+        </div>
+    </div>
+    
+
+    </>
+   )
+  }
